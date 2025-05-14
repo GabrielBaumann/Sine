7aa72f88edc04c0a642f2f7dc342cab03049bb6a
Tiago Elrick <elrickall123@gmail.com>
2025-05-14 10:49:41 -0300

    teste

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require __DIR__ . "/vendor/autoload.php";
 use Source\Core\Session;
 use CoffeeCode\Router\Router;
 
+echo "hello world";
+
 $session = new Session();
 $route = new Router(url(), ":");
 
