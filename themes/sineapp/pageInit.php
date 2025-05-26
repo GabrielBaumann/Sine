b9fec12d99954ec9ef30e6afdaaa0ecb8a32a1de
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-05-26 14:47:25 -0300

    Feat & Fix: Search bar blue ring

--- themes/sineapp/pageInit.php
+++ themes/sineapp/pageInit.php
@@ -43,19 +43,6 @@
     </div>
 </div>
 
-        <!-- Search Bar -->
-        <div class="relative mb-4 lg:mb-6">
-            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <i class="fas fa-search text-gray-400"></i>
-            </div>
-            <input type="text" class="block w-full pl-10 pr-12 py-2 border border-gray-300 rounded-lg bg-white focus:outline-none focus:ring-1 focus:ring-sine-500 focus:border-sine-500" placeholder="Pesquisar candidatos...">
-            <button class="cursor-pointer absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-gray-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                </svg>
-            </button>
-        </div>
-
         <div class="bg-transparent p-4 lg:p-6">
   <div class="flex justify-between items-center mb-3">
     <h3 class="text-base lg:text-lg font-normal text-gray-800">Candidatos Recentes</h3>
