a1a88d3484683706c53da224b0786b35e3fe878f
Lucas Silva <trabalhovba@gmail.com>
2025-05-28 18:09:40 -0300

    feat: update

--- themes/sineweb/layout.php
+++ themes/sineweb/layout.php
@@ -4,14 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $this->e($title)?></title>
-<<<<<<< HEAD
   <link rel="stylesheet" href="src/output.css">
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-=======
-  <!-- <link rel="stylesheet" href="src/output.css"> -->
-   <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
->>>>>>> b431014f6f696535af52818179cc42c9ab0deb3d
   <link rel="stylesheet" href="<?= theme("/assets/css/message.css")?>">
 </head>
 <body class="h-screen flex flex-col md:flex-row bg-gradient-to-b from-white to-blue-200">
