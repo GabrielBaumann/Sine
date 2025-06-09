4c820efc71929848d2c57e342fb7ea78366c667c
LucasNZ <trabalhovba@gmail.com>
2025-06-09 09:03:46 -0300

    Feat: delete pageInit

--- themes/sineapp/pageVacancy.php
+++ themes/sineapp/pageVacancy.php
@@ -46,4 +46,4 @@
 
 <?php $this->start("scripts"); ?>
   <script src="<?= theme("/assets/js/vacancy/page.js", CONF_VIEW_APP) ?>"></script>
-<?php $this->stop("scrpts"); ?>
+<?php $this->stop("scripts"); ?>
