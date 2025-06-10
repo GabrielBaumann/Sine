00419e7e8c6bd3430b5044320f1febc2e2556114
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-06-10 10:43:16 -0300

    Feat: New Vacancy Forms

--- themes/sineapp/pageVacancy.php
+++ themes/sineapp/pageVacancy.php
@@ -2,44 +2,12 @@
 
 <!-- Conteúdo principal -->
 <main class="flex-1 overflow-y-auto p-6 pb-20 lg:pb-6">
-    <!-- Cabeçalho com título e botão -->
-    <div class="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mt-10">
-        <h1 class="text-2xl font-bold text-gray-800">Lista de Vagas</h1>
-        <div class="flex flex-col sm:flex-row gap-3">
-            <!-- Filtros -->
-            <div class="flex gap-2">
-                <select 
-                    name="search-enterprise"
-                    data-url="<?= url("/pesquisarvagas") ?>"
-                    data-ajax="listVacancy"
-                    class="input-search bg-white border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <option value="*">Todas</option>
-                    <?php foreach($listEnterprise as $entreprise): ?>
-                        <option value="<?= $entreprise->id_enterprise ?>" ><?= $entreprise->name_enterprise ?></option>
-                    <?php endforeach; ?>
-                </select>
-                <select 
-                    name="search-all-tatus"
-                    data-url="<?= url("/pesquisarvagas") ?>"
-                    data-ajax="listVacancy"
-                    class="input-search bg-white border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
-                    <option value="*">Todos Status</option>
-                    <option>Ativa</option>
-                    <option>Encerrada</option>
-                </select>
-                <button class="cursor-pointer flex bg-blue-700 px-3 py-2 text-white font-semibold rounded-lg">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                    </svg>
-                    Nova vaga
-                </button>
-            </div>
-        </div>
-    </div>
+    
 
     <!-- Tabela Responsiva -->
     <div id="listVacancy">
-        <?php $this->insert("/pageVacancy/listVacancy"); ?>
+      <!-- Alterei para mostrar o formulário, mas deve ser mostrado apenas a tabela nessa pagina atual -->
+        <?php $this->insert("/pageVacancy/formsNewVacancy"); ?>
     </div>
     
 </main>
--- themes/sineapp/pageVacancy/listVacancy.php
+++ themes/sineapp/pageVacancy/listVacancy.php
@@ -2,8 +2,42 @@
 use Source\Models\Enterprise;
 $entreprise = new Enterprise();
 ?>
-
+<!-- Cabeçalho com título e botão -->
+    <div class="flex flex-col md:flex-row md:items-center md:justify-between gap-4 mb-10">
+        <h1 class="text-2xl font-bold text-gray-800">Lista de Vagas</h1>
+        <div class="flex flex-col sm:flex-row gap-3">
+            <!-- Filtros -->
+            <div class="flex gap-2">
+                <select 
+                    name="search-enterprise"
+                    data-url="<?= url("/pesquisarvagas") ?>"
+                    data-ajax="listVacancy"
+                    class="input-search bg-white border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <option value="*">Todas</option>
+                    <?php foreach($listEnterprise as $entreprise): ?>
+                        <option value="<?= $entreprise->id_enterprise ?>" ><?= $entreprise->name_enterprise ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <select 
+                    name="search-all-tatus"
+                    data-url="<?= url("/pesquisarvagas") ?>"
+                    data-ajax="listVacancy"
+                    class="input-search bg-white border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500">
+                    <option value="*">Todos Status</option>
+                    <option>Ativa</option>
+                    <option>Encerrada</option>
+                </select>
+                <button class="cursor-pointer flex bg-blue-700 px-3 py-2 text-white font-semibold rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    Nova vaga
+                </button>
+            </div>
+        </div>
+    </div>
 <div class="bg-transparent rounded-md overflow-hidden mt-10">
+    
     <div class="overflow-x-auto">
         <table class="min-w-full divide-y divide-gray-200 responsive-table">
             <thead class="bg-gradient-to-r from-blue-500 to-blue-800 text-white">
