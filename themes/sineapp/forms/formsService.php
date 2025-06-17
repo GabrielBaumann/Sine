8186932e373e50110a42d22925a9ac730d944df7
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-06-17 14:13:16 -0300

    Feat: Add new fields to the servide forms

--- themes/sineapp/forms/formsService.php
+++ themes/sineapp/forms/formsService.php
@@ -11,7 +11,7 @@
         </p>
     </div>
 
-    <h1 id="titleForm" class="text-2xl font-semibold text-gray-900 md:px-3 py-4 md:py-10"><?= $titleForm ?? "" ?></h1>
+    <h1 id="titleForm" class="text-2xl font-semibold text-gray-900 md:px-3 py-3"><?= $titleForm ?? "" ?></h1>
 
     <form id="formService" action="<?= url("/formularioAtendimento") . (isset($worker->id_worker) ? "/" . $worker->id_worker : "" ) ; ?>" method="post" class="md:p-3 grid grid-cols-1 md:grid-cols-4 gap-4">
         <?= csrf_input(); ?>
@@ -92,6 +92,62 @@
             </select>
         </div>
 
+        <!-- Encaminhamento para entrevistas -->
+        <div class="col-span-4 md:col-span-2 lg:col-span-1">
+            <label for="data-atendimento" class="block text-sm font-medium text-gray-700 mb-1">CNPJ *</label>
+            <input 
+                value=""
+                type="text" 
+                id="" 
+                name="" 
+                class="bg-white w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-sine-500 focus:border-sine-500" >
+        </div>
+
+        <!-- Encaminhamento para entrevistas -->
+        <div class="col-span-4 md:col-span-2 lg:col-span-2">
+            <label for="data-atendimento" class="block text-sm font-medium text-gray-700 mb-1">Empresa *</label>
+            <input 
+                value=""
+                type="text" 
+                id="" 
+                name="" 
+                class="bg-white w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-sine-500 focus:border-sine-500" >
+        </div>
+
+        <!-- Encaminhamento para entrevistas -->
+        <div class="col-span-4 md:col-span-2 lg:col-span-1">
+            <label for="cpf" class="block text-sm font-medium text-gray-700 mb-1">Telefone *</label>
+            <input
+                value="" 
+                data-url=""
+                type="text" 
+                id="" 
+                name="" 
+                class="bg-white w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-sine-500 focus:border-sine-500" 
+                placeholder="000.000.000-00">
+        </div>
+
+        <!-- Encaminhamento para entrevistas -->
+        <div class="col-span-4 md:col-span-2 lg:col-span-1">
+            <label for="cterc" class="block text-sm font-medium text-gray-700 mb-1">Função *</label>
+            <select id="cterc" name="cterc" class="bg-white mt-1 block w-full pl-3 pr-10 py-2 text-base border border-gray-300 focus:outline-none focus:ring-sine-500 focus:border-sine-500 sm:text-sm rounded-md">
+                <option>Selecione</option>
+            </select>
+        </div>
+
+        <!-- Encaminhamento para entrevistas -->
+        <div class="col-span-4 md:col-span-2 lg:col-span-2">
+            <label for="nome" class="block text-sm font-medium text-gray-700 mb-1">Responsável *</label>
+            <input 
+                value=""
+                type="text" 
+                id="" 
+                name="" 
+                class="bg-white w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-sine-500 focus:border-sine-500" 
+                placeholder="Digite o nome completo" >
+        </div>
+
+        <!-- linha inteira -->
         <div class="col-span-4">
             <label for="observation" class="block text-sm font-medium text-gray-700 mb-1">Observação</label>
             <textarea 
