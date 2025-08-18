055bd0d85d6faca3bd290888af71cf03a72e87bc
Gabriel Baumann <gabrielgoncalves238@gmail.com>
2025-08-18 10:27:07 -0300

    Chore: Added favicon

--- themes/sineweb/layout.php
+++ themes/sineweb/layout.php
@@ -7,6 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="icon" type="image/png" href="<?= theme("/assets/images/favicon_sine.png")?>">
     <link rel="stylesheet" href="<?= theme("/assets/css/message.css")?>">
     <script>
         tailwind.config = {
